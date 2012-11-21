6bacb7e1babf4bfae9444e013fd45a2d3f7ad8ac
Nyte <saasbook@nyte.(none)>
2012-11-20 20:15:25 -0800

    Implemented strcopy framework.  Psuedocode is tested.

--- strcopy.py
+++ strcopy.py
@@ -20,7 +20,11 @@ def identity(item):
   return item
 
 def strcopy(string):
-  pass
+  new_string_arr = empty_list()
+  for index in array_range(string):
+    add_to_list(new_string_arr, get_n_item(string, index))
+  new_string = identity(combine_array(new_string_arr))
+  return new_string
 
 if __name__ == '__main__':
   arr = 'Test_string'
