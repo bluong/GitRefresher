7df61dcf9f72d9b156c63ae23e6440d584543a56
Nyte <saasbook@nyte.(none)>
2012-11-21 16:27:30 -0800

    Moving Soldier class to its own python file.

--- battle_runner.py
+++ battle_runner.py
@@ -1,26 +1,8 @@
 from battle import Battle
-import random
 
 SOLDIER_FILE = "soldiers.txt"
 SKILL_FILE = "skills.txt"
 
-class Soldier:
-    def __init__(self, health, skill_dict):
-        self.health = health
-        self.skills = skill_dict
-
-    def choose_skill(self):
-        return self.skills[random.choice(self.skills.keys())]
-      
-    def receive_damage(self, dmg):
-        self.health -= dmg
-        
-    def regenerate(self, life):
-        self.health += life
-        
-    def is_incapacitated(self):
-        return self.health <= 0
-  
 def battle_start():
     skirmish = Battle(SOLDIER_FILE, SKILL_FILE)
     while (not skirmish.gameover()):
